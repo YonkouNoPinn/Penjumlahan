ff78ebc99b75492c3420928c81c2194f4b53445f
Kelvinn XTkj2 <144640412+YonkouNoPinn@users.noreply.github.com>
2023-09-11 09:21:30 +0700

    Create Penjumlahan.cpp

---
+++ Penjumlahan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+int a,b,c;
+cout<<"PEMROGAMAN OPERASI PENJUMLAHAN \n";
+cout<<"masukkan angka pertama: ";
+cin>>a;
+cout<<"masukkan angka kedua: ";
+cin>>b;
+c=a+b;
+cout<<"hasilnya adalah: "<<c<<endl;
+return 0;
+}
